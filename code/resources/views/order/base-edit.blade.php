2bad48ef6f13ca74be9650a08a18c2155893a528
Roberto Guido <bob4job@gmail.com>
2016-01-20 02:33:50 +0100

    altre integrazioni su interazione con permessi diversi

--- code/resources/views/order/base-edit.blade.php
+++ code/resources/views/order/base-edit.blade.php
@@ -1,4 +1,15 @@
-@include('commons.selectobjfield', ['obj' => $order, 'name' => 'supplier_id', 'objects' => App\Supplier::orderBy('name', 'asc')->get(), 'label' => 'Fornitore', 'mandatory' => true])
+@include('commons.selectobjfield', [
+	'obj' => $order,
+	'name' => 'supplier_id',
+	'label' => 'Fornitore',
+	'mandatory' => true,
+	'objects' => App\Supplier::whereHas('permissions', function($query) {
+		$query->where('action', '=', 'supplier.orders')->where(function($query) {
+			$query->where('user_id', '=', Auth::user()->id)->orWhere('user_id', '=', '*');
+		});
+	})->orderBy('name', 'asc')->get()
+])
+
 @include('commons.datefield', ['obj' => $order, 'name' => 'start', 'label' => 'Data Apertura', 'mandatory' => true])
 @include('commons.datefield', ['obj' => $order, 'name' => 'end', 'label' => 'Data Chiusura', 'mandatory' => true])
 @include('commons.datefield', ['obj' => $order, 'name' => 'shipping', 'label' => 'Data Consegna'])
---
+++ code/resources/views/user/show.blade.php
@@ -0,0 +1,14 @@
+<form class="form-horizontal main-form" method="PUT" action="{{ url('users/' . $user->id) }}">
+	<div class="row">
+		<div class="col-md-6">
+			@include('commons.staticstringfield', ['obj' => $user, 'name' => 'name', 'label' => 'Nome', 'mandatory' => true])
+			@include('commons.staticstringfield', ['obj' => $user, 'name' => 'surname', 'label' => 'Cognome', 'mandatory' => true])
+			@include('commons.staticstringfield', ['obj' => $user, 'name' => 'phone', 'label' => 'Telefono'])
+			@include('commons.staticstringfield', ['obj' => $user, 'name' => 'email', 'label' => 'E-Mail'])
+			@include('commons.staticdatefield', ['obj' => $user, 'name' => 'birthday', 'label' => 'Data di Nascita'])
+		</div>
+		<div class="col-md-6">
+			@include('commons.staticdatefield', ['obj' => $user, 'name' => 'member_since', 'label' => 'Membro da'])
+		</div>
+	</div>
+</form>
