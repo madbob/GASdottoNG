e5ec41f308556460a613fdc9e6883d99f15f17f5
pcdevoti <piercarlo.devoti@cortex.it>
2016-02-18 16:42:43 +0100

    Update 2015_04_29_130341_create_table_local_files.php

--- code/database/migrations/2015_04_29_130341_create_table_local_files.php
+++ code/database/migrations/2015_04_29_130341_create_table_local_files.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocalFiles extends Migration
+class CreateLocalFilesTable extends Migration
 {
 
     /**
