9acaedbc93dc7ff08c4e35ada5d9602acaa2b439
pcdevoti <piercarlo.devoti@cortex.it>
2016-02-21 16:20:09 +0100

    Update 2016_01_10_194635_create_balances_table.php

--- code/database/migrations/2016_01_10_194635_create_balances_table.php
+++ code/database/migrations/2016_01_10_194635_create_balances_table.php
@@ -11,15 +11,18 @@ class CreateBalancesTable extends Migration
                         $table->increments('id');
                         $table->timestamps();
 
-                        $table->string('target_type');
-                        $table->string('target_id');
-                        $table->date('date');
-                        $table->decimal('cash_amount', 5, 2)->default(0);
-                        $table->decimal('bank_amount', 5, 2)->default(0);
-                        $table->decimal('orders_amount', 5, 2)->default(0);
-                        $table->decimal('deposits_amount', 5, 2)->default(0);
+                        $table->string('target_type',1); /* G = GAS, U = User, S = Supplier */
+                        $table->string('target_id',20);
+                        $table->datetime('date');
+                        $table->decimal('balance', 6, 2)->default(0);
+                        $table->decimal('cash_amount', 6, 2)->default(0);
+                        $table->decimal('bank_amount', 6, 2)->default(0);
+                        $table->decimal('orders_amount', 6, 2)->default(0);
+                        $table->decimal('deposits_amount', 6, 2)->default(0);
 
+                        
                         $table->index('id');
+                        $table->index(['target_type','target_id']);
                 });
         }
 
