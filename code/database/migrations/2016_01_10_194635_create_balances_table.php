2c054d2010165b43e56808b627f4fd9187af54e0
pcdevoti <piercarlo.devoti@cortex.it>
2016-02-21 16:23:14 +0100

    Update 2016_01_10_194635_create_balances_table.php

--- code/database/migrations/2016_01_10_194635_create_balances_table.php
+++ code/database/migrations/2016_01_10_194635_create_balances_table.php
@@ -15,12 +15,11 @@ class CreateBalancesTable extends Migration
                         $table->string('target_id',20);
                         $table->datetime('date');
                         $table->decimal('balance', 6, 2)->default(0);
-                        $table->decimal('cash_amount', 6, 2)->default(0);
-                        $table->decimal('bank_amount', 6, 2)->default(0);
-                        $table->decimal('orders_amount', 6, 2)->default(0);
-                        $table->decimal('deposits_amount', 6, 2)->default(0);
+                        $table->decimal('bank_balance',6,2)->default(0);
+			$table->decimal('cash_balance',6,2)->default(0);
+			$table->decimal('suppliers_balance',6,2)->default(0);
+			$table->decimal('deposit_balance',6,2)->default(0);
 
-                        
                         $table->index('id');
                         $table->index(['target_type','target_id']);
                 });
