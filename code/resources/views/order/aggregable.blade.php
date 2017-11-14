df5e1dcce4e7ac8e09cd978fd362d7b36c5b7ca5
Roberto Guido <bob4job@gmail.com>
2017-11-14 17:51:26 +0100

    piccola modifica in pannello ordini aggregabili

--- code/resources/views/order/aggregable.blade.php
+++ code/resources/views/order/aggregable.blade.php
@@ -1,8 +1,10 @@
-<ul class="well" data-aggregate-id="new">
-    <!-- Questo viene lasciato deliberatamente vuoto per poter fungere da appoggio per la creazione di un nuovo aggregato -->
-</ul>
+@foreach($orders as $index => $order)
+    @if($index % 5 == 0)
+        <ul class="well" data-aggregate-id="new">
+            <!-- Questo viene lasciato deliberatamente vuoto per poter fungere da appoggio per la creazione di un nuovo aggregato -->
+        </ul>
+    @endif
 
-@foreach($orders as $order)
     <ul class="well" data-aggregate-id="{{ $order->id }}">
         @foreach($order->orders as $suborder)
             <li data-order-id="{{ $suborder->id }}">
