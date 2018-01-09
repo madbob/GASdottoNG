fd76a1e38c9940d9887e5ca441a9e422ee7c3bcb
Roberto Guido <bob4job@gmail.com>
2018-01-09 23:17:46 +0100

    spostato fix database in middleware dedicato

--- code/app/Providers/AppServiceProvider.php
+++ code/app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-use App\Measure;
-use App\Category;
 use App\User;
 use App\Role;
 use App\Delivery;
@@ -14,24 +12,6 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        /*
-            Qui faccio in modo di avere sempre dei default.
-            Serve solo per sistemare le istanze già esistenti in cui questi
-            valori sono stato rimossi prima del blocco a livello di
-            amministrazione, codice da rimuovere tra qualche tempo
-            Addì: 09/01/2018
-        */
-        if (Measure::find('non-specificato') == null) {
-            $measure = new Measure();
-            $measure->name = _i('Non Specificato');
-            $measure->save();
-        }
-        if (Category::find('non-specificato') == null) {
-            $category = new Category();
-            $category->name = _i('Non Specificato');
-            $category->save();
-        }
-
         User::created(function($user) {
             $default_roles = Role::where('always', true)->get();
             foreach($default_roles as $dr) {
