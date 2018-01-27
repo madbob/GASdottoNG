67804f09b9cf0e53aa9cebfdc39447724fac18fc
Roberto Guido <bob4job@gmail.com>
2018-01-27 19:14:42 +0100

    micro fix in ordini e consegne

--- code/app/Order.php
+++ code/app/Order.php
@@ -315,10 +315,8 @@ class Order extends Model
         $summary->transport = $total_transport + $order->transport;
 
         $total_transport_delivered = 0;
-        if ($order->transport > 0) {
-            foreach ($order->bookings()->where('status', 'shipped')->get() as $shipped_booking)
-                $total_transport_delivered += $shipped_booking->transport;
-        }
+        foreach ($order->bookings()->where('status', 'shipped')->get() as $shipped_booking)
+            $total_transport_delivered += $shipped_booking->transport;
         $summary->transport_delivered = $total_transport_delivered;
 
         $summary->notes = [];
