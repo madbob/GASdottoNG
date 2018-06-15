b0a4643276bf1abf2249e5bb0706b0438476cdd5
Roberto Guido <bob4job@gmail.com>
2018-06-15 02:51:05 +0200

    traduzioni pannello varianti

--- code/resources/views/product/edit.blade.php
+++ code/resources/views/product/edit.blade.php
@@ -2,18 +2,18 @@
     <div class="row">
         <div class="col-md-6">
             @include('product.base-edit', ['product' => $product])
-            @include('commons.textfield', ['obj' => $product, 'name' => 'supplier_code', 'label' => 'Codice Fornitore'])
-            @include('commons.boolfield', ['obj' => $product, 'name' => 'active', 'label' => 'Ordinabile'])
+            @include('commons.textfield', ['obj' => $product, 'name' => 'supplier_code', 'label' => _i('Codice Fornitore')])
+            @include('commons.boolfield', ['obj' => $product, 'name' => 'active', 'label' => _i('Ordinabile')])
         </div>
         <div class="col-md-6">
             @include('commons.imagefield', ['obj' => $product, 'name' => 'picture', 'label' => _i('Foto'), 'valuefrom' => 'picture_url'])
-            @include('commons.decimalfield', ['obj' => $product, 'name' => 'portion_quantity', 'label' => 'Pezzatura', 'decimals' => 3])
-            @include('commons.boolfield', ['obj' => $product, 'name' => 'variable', 'label' => 'Variabile'])
-            @include('commons.decimalfield', ['obj' => $product, 'name' => 'package_size', 'label' => 'Confezione', 'decimals' => 3])
-            @include('commons.decimalfield', ['obj' => $product, 'name' => 'multiple', 'label' => 'Multiplo', 'decimals' => 3])
-            @include('commons.decimalfield', ['obj' => $product, 'name' => 'min_quantity', 'label' => 'Minimo', 'decimals' => 3])
-            @include('commons.decimalfield', ['obj' => $product, 'name' => 'max_quantity', 'label' => 'Massimo Consigliato', 'decimals' => 3])
-            @include('commons.decimalfield', ['obj' => $product, 'name' => 'max_available', 'label' => 'Disponibile', 'decimals' => 3])
+            @include('commons.decimalfield', ['obj' => $product, 'name' => 'portion_quantity', 'label' => _i('Pezzatura'), 'decimals' => 3])
+            @include('commons.boolfield', ['obj' => $product, 'name' => 'variable', 'label' => _i('Variabile')])
+            @include('commons.decimalfield', ['obj' => $product, 'name' => 'package_size', 'label' => _i('Confezione'), 'decimals' => 3])
+            @include('commons.decimalfield', ['obj' => $product, 'name' => 'multiple', 'label' => _i('Multiplo'), 'decimals' => 3])
+            @include('commons.decimalfield', ['obj' => $product, 'name' => 'min_quantity', 'label' => _i('Minimo'), 'decimals' => 3])
+            @include('commons.decimalfield', ['obj' => $product, 'name' => 'max_quantity', 'label' => _i('Massimo Consigliato'), 'decimals' => 3])
+            @include('commons.decimalfield', ['obj' => $product, 'name' => 'max_available', 'label' => _i('Disponibile'), 'decimals' => 3])
             @include('product.variantseditor', ['product' => $product])
         </div>
     </div>
@@ -21,7 +21,7 @@
     @include('commons.formbuttons', [
         'left_buttons' => [
             (object) [
-                'label' => 'Duplica',
+                'label' => _i('Duplica'),
                 'url' => '',
                 'class' => 'duplicate-product'
             ]
@@ -38,26 +38,26 @@
 
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Crea/Modifica Variante</h4>
+                    <h4 class="modal-title">{{ _i('Crea/Modifica Variante') }}</h4>
                 </div>
                 <div class="modal-body">
-                    @include('commons.textfield', ['obj' => null, 'name' => 'name', 'label' => 'Nome', 'mandatory' => true])
-                    @include('commons.boolfield', ['obj' => null, 'name' => 'has_offset', 'label' => 'Differenza Prezzo'])
+                    @include('commons.textfield', ['obj' => null, 'name' => 'name', 'label' => _i('Nome'), 'mandatory' => true])
+                    @include('commons.boolfield', ['obj' => null, 'name' => 'has_offset', 'label' => _i('Differenza Prezzo')])
 
                     <div class="form-group">
-                        <label class="col-sm-{{ $labelsize }} control-label">Valori</label>
+                        <label class="col-sm-{{ $labelsize }} control-label">{{ _i('Valori') }}</label>
 
                         <div class="col-sm-{{ $fieldsize }} values_table">
                             @include('commons.manyrows', [
                                 'contents' => null,
                                 'columns' => [
                                     [
-                                        'label' => 'Valore',
+                                        'label' => _i('Valore'),
                                         'field' => 'value',
                                         'type' => 'text'
                                     ],
                                     [
-                                        'label' => 'Differenza Prezzo',
+                                        'label' => _i('Differenza Prezzo'),
                                         'field' => 'price_offset',
                                         'type' => 'decimal',
                                         'extra' => [
@@ -70,8 +70,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Annulla</button>
-                    <button type="submit" class="btn btn-success">Salva</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ _i('Annulla') }}</button>
+                    <button type="submit" class="btn btn-success">{{ _i('Salva') }}</button>
                 </div>
             </form>
         </div>
