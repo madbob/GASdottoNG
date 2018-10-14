3d70da9393b414afcb065c548e14b3769508e367
Roberto Guido <bob4job@gmail.com>
2018-10-14 18:13:01 +0200

    inoltro mail delle fatture generate

--- code/resources/views/receipt/edit.blade.php
+++ code/resources/views/receipt/edit.blade.php
@@ -1,13 +1,13 @@
 <form class="form-horizontal main-form receipt-editor" method="PUT" action="{{ route('receipts.update', $receipt->id) }}">
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-4">
             @include('commons.staticobjfield', ['obj' => $receipt, 'name' => 'user', 'label' => _i('Utente')])
             @include('commons.staticstringfield', ['obj' => $receipt, 'name' => 'number', 'label' => _i('Numero')])
             @include('commons.datefield', ['obj' => $receipt, 'name' => 'date', 'label' => _i('Data')])
             @include('commons.staticpricefield', ['obj' => $receipt, 'name' => 'total', 'label' => _i('Totale Imponibile')])
             @include('commons.staticpricefield', ['obj' => $receipt, 'name' => 'total_vat', 'label' => _i('Totale IVA')])
         </div>
-        <div class="col-md-6">
+        <div class="col-md-4">
             <div class="form-group">
                 <label for="bookings" class="col-sm-{{ $labelsize }} control-label">{{ _i('Prenotazioni Coinvolte') }}</label>
 
@@ -24,15 +24,40 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-4">
+            <div class="list-group pull-right">
+                <a href="#" class="list-group-item" data-toggle="modal" data-target="#receipt-document-{{ $receipt->id }}">{{ _i('Scarica o Inoltra') }}</a>
+            </div>
+        </div>
     </div>
 
-    @include('commons.formbuttons', [
-        'left_buttons' => [
-            (object)[
-                'url' => route('receipts.download', $receipt->id),
-                'class' => '',
-                'label' => _i('Scarica')
-            ]
-        ]
-    ])
+    @include('commons.formbuttons')
 </form>
+
+<div class="modal fade close-on-submit order-document-download-modal" id="receipt-document-{{ $receipt->id }}" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-extra-lg" role="document">
+        <div class="modal-content">
+            <form class="form-horizontal direct-submit" method="GET" action="{{ route('receipts.download', $receipt->id) }}" data-toggle="validator" novalidate>
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">{{ _i('Scarica o Inoltra') }}</h4>
+                </div>
+                <div class="modal-body">
+                    <p>
+                        {{ _i("Scarica la fattura generata, o inoltrala via email.") }}
+                    </p>
+
+                    @include('order.filesmail', [
+                        'contacts' => $receipt->user->contacts()->where('type', 'email')->get(),
+                        'default_subject' => _i('Nuova fattura da %s', [$currentgas->name]),
+                        'default_text' => _i("In allegato l'ultima fattura da %s.", [$currentgas->name])
+                    ])
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">{{ _i('Annulla') }}</button>
+                    <button type="submit" class="btn btn-success">{{ _i('Download') }}</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
