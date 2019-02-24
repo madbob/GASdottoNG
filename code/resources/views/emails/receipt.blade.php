98a038e25792ee82122205e896e5af5342f8ccae
Roberto Guido <bob4job@gmail.com>
2019-02-24 23:07:17 +0100

    rimossi files non piu' utilizzati

--- code/resources/views/emails/receipt.blade.php
+++
@@ -1,7 +0,0 @@
-<p>
-    @if(empty($txt_message))
-        {{ _i("In allegato l'ultima fattura da %s.", [$currentgas->name]) }}
-    @else
-        {{ nl2br($txt_message) }}
-    @endif
-</p>
--- code/resources/views/emails/resetpassword.blade.php
+++
@@ -1,7 +0,0 @@
-<p>
-    {{ _i("È stato chiesto l'aggiornamento della tua password su GASdotto.") }}
-</p>
-
-<p>
-    {!! _i('<a href="%s">Clicca qui per aggiornare la tua password</a>, o ignora la mail se non hai chiesto tu questa operazione.', [$url]) !!}
-</p>
