38c6a47d747746006e05f419226647451fb531ad
Federico Fissore <federico@fissore.org>
2019-02-27 16:58:22 +0100

    By not requiring port and container name when running tests, we can run them while running the app

--- code/test.sh
+++ code/test.sh
@@ -9,11 +9,8 @@ fi
 docker run \
 	-t -i \
 	--rm \
-	-p 8000:8000 \
-	-h dev.gasdotto.org \
 	-v $(pwd):/app \
 	-v $(pwd)/_docker/.docker_env:/app/.env \
 	-v $(pwd)/.docker_bundle:/home/username/.composer \
-	--name=dev-gasdotto-org \
 	gasdotto/dev-gasdotto-org \
 	vendor/phpunit/phpunit/phpunit $FILTER
