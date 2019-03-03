465f9f667a4a8ea8c63ad1156b95992a924a4451
Roberto Guido <bob4job@gmail.com>
2019-03-03 21:45:19 +0100

    totale merci e trasporto in documento "dettaglio consegne"

--- code/resources/views/documents/order_shipping.blade.php
+++ code/resources/views/documents/order_shipping.blade.php
@@ -2,7 +2,16 @@
     <body>
         <h3>{{ _i('Dettaglio Consegne Ordine %s presso %s del %s', [$order->internal_number, $order->supplier->printableName(), $order->shipping ? date('d/m/Y', strtotime($order->shipping)) : date('d/m/Y')]) }}</h3>
 
-        <hr/>
+        <br/>
+        <hr>
+        <br/>
+
+        <?php
+
+        $total = 0;
+        $total_transport = 0;
+
+        ?>
 
         @foreach($bookings as $booking)
             <table border="1" style="width: 100%" cellpadding="5" nobr="true">
@@ -27,6 +36,10 @@
                         if (!empty($contacts))
                             $head .= ' - ' . join(', ', $contacts);
 
+                        $booking_total = $booking->total_value;
+                        $total += $booking_total;
+                        $total_transport += $booking->check_transport;
+
                         ?>
                         <strong>{{ $head }}</strong>
                     </th>
@@ -38,11 +51,23 @@
                 ])
 
                 <tr>
-                    <th colspan="3"><strong>{{ _i('Totale') }}: {{ printablePriceCurrency($booking->total_value, ',') }}</strong></th>
+                    <th colspan="3"><strong>{{ _i('Totale') }}: {{ printablePriceCurrency($booking_total, ',') }}</strong></th>
                 </tr>
             </table>
 
             <p>&nbsp;</p>
         @endforeach
+
+        <table border="1" style="width: 100%" cellpadding="5" nobr="true">
+            <tr>
+                <th colspan="3"><strong>{{ _i('Totale') }}: {{ printablePriceCurrency($total, ',') }}</strong></th>
+            </tr>
+
+            @if(!empty($total_transport))
+                <tr>
+                    <th colspan="3"><strong>{{ _i('Trasporto') }}: {{ printablePriceCurrency($total_transport, ',') }}</strong></th>
+                </tr>
+            @endif
+        </table>
     </body>
 </html>
