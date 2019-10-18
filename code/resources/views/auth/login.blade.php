56e0da70e367c798e85349bba7ced901c08cb3d0
Roberto Guido <bob4job@gmail.com>
2019-10-18 15:24:50 +0200

    flag "ricordami" implicito

--- code/resources/views/auth/login.blade.php
+++ code/resources/views/auth/login.blade.php
@@ -37,6 +37,7 @@
 
     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="hidden" name="remember" value="1">
 
         <div class="form-group">
             <label class="col-sm-2 control-label">{{ _i('Username') }}</label>
@@ -66,16 +67,6 @@
                 </div>
             </div>
         @endif
-
-        <div class="form-group">
-            <div class="col-sm-offset-2 col-sm-10">
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" name="remember"> {{ _i('Ricordami') }}
-                    </label>
-                </div>
-            </div>
-        </div>
 
         <br>
 
