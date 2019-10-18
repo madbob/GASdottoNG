dae0815e1514956a341f583f7bd5447ceb1a2476
Roberto Guido <bob4job@gmail.com>
2019-10-18 15:24:29 +0200

    tabella totale in documento dettaglio consegne per utente

--- code/resources/views/documents/personal_aggregate_shipping.blade.php
+++ code/resources/views/documents/personal_aggregate_shipping.blade.php
@@ -17,7 +17,7 @@
             @endif
         </h3>
 
-        <?php $total_booked = $total_delivered = 0 ?>
+        <?php $valid_bookings = 0 ?>
 
         @foreach($bookings as $super_booking)
             @if($super_booking->total_value == 0 && $super_booking->total_delivered == 0)
@@ -42,6 +42,7 @@
 
                             $booked_cell_value += $booking->getValue('booked', false);
                             $delivered_cell_value += $booking->getValue('delivered', false);
+                            $valid_bookings++;
 
                             ?>
 
@@ -92,11 +93,9 @@
                     @endif
 
                     <tr>
-                        <?php $total_booked += $booked_cell_value ?>
                         <th colspan="5"><strong>{{ _i('Totale Prenotato') }}: {{ printablePriceCurrency($booked_cell_value, ',') }}</strong></th>
                     </tr>
                     <tr>
-                        <?php $total_delivered += $delivered_cell_value ?>
                         <th colspan="5"><strong>{{ _i('Totale Consegnato') }}: {{ printablePriceCurrency($delivered_cell_value, ',') }}</strong></th>
                     </tr>
 
@@ -111,15 +110,94 @@
             <p>&nbsp;</p>
         @endforeach
 
-        <table border="1" style="width: 100%" cellpadding="5" nobr="true">
-            <tbody>
-                <tr>
-                    <th colspan="5"><strong>{{ _i('Totale Complessivo Prenotato') }}: {{ printablePriceCurrency($total_booked, ',') }}</strong></th>
-                </tr>
-                <tr>
-                    <th colspan="5"><strong>{{ _i('Totale Complessivo Consegnato') }}: {{ printablePriceCurrency($total_delivered, ',') }}</strong></th>
-                </tr>
-            </tbody>
-        </table>
+        @if($valid_bookings > 1)
+            @foreach($bookings as $super_booking)
+                @if($super_booking->user->isFriend())
+                    @continue
+                @endif
+
+                <?php $booked_cell_value = $delivered_cell_value = 0 ?>
+
+                <table border="1" style="width: 100%" cellpadding="5" nobr="true">
+                    <thead>
+                        <tr>
+                            <th colspan="5">
+                                <strong>{{ _i('Totale') }}</strong>
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach($super_booking->bookings as $booking)
+                            @if($booking->products_with_friends->isEmpty() == false)
+                                <?php
+
+                                $booked_cell_value += $booking->getValue('booked', true);
+                                $delivered_cell_value += $booking->getValue('delivered', true);
+
+                                ?>
+
+                                <tr>
+                                    <td colspan="5"><strong>{{ $booking->order->supplier->printableName() }}</strong></td>
+                                </tr>
+
+                                <tr>
+                                    <td width="40%"><strong>{{ _i('Prodotto') }}</strong></td>
+                                    <td width="15%"><strong>{{ _i('Prenotato') }}</strong></td>
+                                    <td width="15%">&nbsp;</td>
+                                    <td width="15%"><strong>{{ _i('Consegnato') }}</strong></td>
+                                    <td width="15%">&nbsp;</td>
+                                </tr>
+
+                                @foreach($booking->products_with_friends as $product)
+                                    @if($product->variants->isEmpty() == false)
+                                        @foreach($product->variants as $variant)
+                                            @if(!empty($variant->quantity) || !empty($variant->delivered))
+                                                <tr>
+                                                    <td>{{ $product->product->printableName() }}</td>
+                                                    <td>{{ printableQuantity($variant->quantity, $product->product->measure->discrete, 2, ',') }} {{ $variant->printableName() }}</td>
+                                                    <td>{{ printablePriceCurrency($variant->quantityValue(), ',') }}</td>
+                                                    <td>{{ printableQuantity($variant->delivered, $product->product->measure->discrete, 2, ',') }} {{ $variant->printableName() }}</td>
+                                                    <td>{{ printablePriceCurrency($variant->deliveredValue(), ',') }}</td>
+                                                </tr>
+                                            @endif
+                                        @endforeach
+                                    @else
+                                        @if(!empty($product->quantity) || !empty($product->delivered))
+                                            <tr>
+                                                <td>{{ $product->product->printableName() }}</td>
+                                                <td>{{ printableQuantity($product->quantity, $product->product->measure->discrete, 2, ',') }}</td>
+                                                <td>{{ printablePriceCurrency($product->quantityValue(), ',') }}</td>
+                                                <td>{{ printableQuantity($product->delivered, $product->product->measure->discrete, 2, ',') }}</td>
+                                                <td>{{ printablePriceCurrency($product->deliveredValue(), ',') }}</td>
+                                            </tr>
+                                        @endif
+                                    @endif
+                                @endforeach
+                            @endif
+                        @endforeach
+
+                        @if(($transport = $booking->getValue('transport', true)) != 0)
+                            <tr>
+                                <th colspan="5"><strong>{{ _i('Trasporto') }}: {{ printablePriceCurrency($transport, ',') }}</th>
+                            </tr>
+                        @endif
+
+                        <tr>
+                            <th colspan="5"><strong>{{ _i('Totale Prenotato') }}: {{ printablePriceCurrency($booked_cell_value, ',') }}</strong></th>
+                        </tr>
+                        <tr>
+                            <th colspan="5"><strong>{{ _i('Totale Consegnato') }}: {{ printablePriceCurrency($delivered_cell_value, ',') }}</strong></th>
+                        </tr>
+
+                        @if(($discount = $booking->getValue('discount', true)) != 0)
+                            <tr>
+                                <th colspan="5"><strong>{{ _i('Sconto') }}: {{ printablePriceCurrency($discount, ',') }}</th>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
+            @endforeach
+        @endif
     </body>
 </html>
