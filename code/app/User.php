5c69d2d8977c993c3757a1b5ecae8144df3807ca
Roberto Guido <bob4job@gmail.com>
2020-04-14 10:12:30 +0200

    fix scemo per reset password

--- code/app/User.php
+++ code/app/User.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Database\Eloquent\Model;
