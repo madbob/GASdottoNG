e784786522cd4ad6a5966fb826e5c4008023ca5f
Roberto Guido <bob4job@gmail.com>
2020-08-26 14:58:57 +0200

    controllo modalita' manutenzione anche per utenti gia' autenticati

--- code/app/Http/Middleware/EnforcePassword.php
+++ code/app/Http/Middleware/EnforcePassword.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Auth;
 use Closure;
 
 use App\Measure;
@@ -13,10 +14,24 @@ class EnforcePassword
     public function handle($request, Closure $next)
     {
         $user = $request->user();
-        if ($user && $user->enforce_password_change) {
-            $route_name = $request->route()->getName();
-            if ($route_name != 'users.password' && $route_name != 'users.update')
-                return redirect()->route('users.password');
+
+        if ($user) {
+            if ($user->enforce_password_change) {
+                $route_name = $request->route()->getName();
+                if ($route_name != 'users.password' && $route_name != 'users.update') {
+                    return redirect()->route('users.password');
+                }
+            }
+
+            /*
+                Oltre che al login, faccio il controllo sull'eventuale modalità
+                di manutenzione attiva anche qui per intercettare gli utenti
+                precedentemente loggati (e magari col flag remember_me settato)
+            */
+            if ($user->gas->restricted == '1' && $user->can('gas.access', $user->gas) == false) {
+                Auth::logout();
+                return redirect(url('login'));
+            }
         }
 
         return $next($request);
