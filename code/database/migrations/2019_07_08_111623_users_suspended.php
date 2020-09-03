7153a16ee02f9798524e54dcb58d674cc71dabef
Roberto Guido <bob4job@gmail.com>
2020-09-03 16:37:09 +0200

    rimosso flag utente e fornitore sospeso da migrazioni db

--- code/database/migrations/2019_07_08_111623_users_suspended.php
+++ code/database/migrations/2019_07_08_111623_users_suspended.php
@@ -15,22 +15,8 @@ class UsersSuspended extends Migration
             $table->date('suspended_at')->nullable()->after('deleted_at')->default(null);
         });
 
-        DB::statement('UPDATE users SET suspended_at = deleted_at WHERE suspended = 1');
-        DB::statement('UPDATE users SET deleted_at = null WHERE suspended = 1');
-
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('suspended');
-        });
-
         Schema::table('suppliers', function (Blueprint $table) {
             $table->date('suspended_at')->nullable()->after('deleted_at')->default(null);
-        });
-
-        DB::statement('UPDATE suppliers SET suspended_at = deleted_at WHERE suspended = 1');
-        DB::statement('UPDATE suppliers SET deleted_at = null WHERE suspended = 1');
-
-        Schema::table('suppliers', function (Blueprint $table) {
-            $table->dropColumn('suspended');
         });
     }
 
