32d6a22787e285edf5539d72f1f5f89fe5b98264
Roberto Guido <bob4job@gmail.com>
2021-03-29 01:15:27 +0200

    data di sospensione/cessazione, default in data odierna

--- code/app/SuspendableTrait.php
+++ code/app/SuspendableTrait.php
@@ -11,12 +11,12 @@ trait SuspendableTrait {
                 $this->deleted_at = null;
                 break;
             case 'suspended':
-                $this->suspended_at = !empty($suspended_at) ? decodeDate($suspended_at) : date('Y-m-d');
+                $this->suspended_at = decodeDate($suspended_at) ?: date('Y-m-d');
                 $this->deleted_at = null;
                 break;
             case 'deleted':
                 $this->suspended_at = null;
-                $this->deleted_at = !empty($deleted_at) ? decodeDate($deleted_at) : date('Y-m-d');
+                $this->deleted_at = decodeDate($deleted_at) ?: date('Y-m-d');
                 break;
         }
     }
