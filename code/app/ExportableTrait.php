7eda4d55713d1396779ab627b02cfb23bfd1e624
Roberto Guido <bob4job@gmail.com>
2021-07-13 11:46:03 +0200

    fix in classe per esportazione

--- code/app/ExportableTrait.php
+++ code/app/ExportableTrait.php
@@ -11,8 +11,8 @@ trait ExportableTrait
         return url('import/gdxp?classname=' . get_class($this) . '&id=' . $this->id);
     }
 
-    abstract public static function exportXML();
+    abstract public function exportXML();
     abstract public static function readXML($xml);
-    abstract public static function exportJSON();
+    abstract public function exportJSON();
     abstract public static function readJSON($json);
 }
