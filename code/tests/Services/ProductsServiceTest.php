98d48eddec1435edd9da24ed5ee0ef9303a3e629
Roberto Guido <bob4job@gmail.com>
2021-08-25 20:06:13 +0200

    fix unit test (gli sconti non stanno piu' direttamente nei prodotti)

--- code/tests/Services/ProductsServiceTest.php
+++ code/tests/Services/ProductsServiceTest.php
@@ -92,14 +92,12 @@ class ProductsServiceTest extends TestCase
         $this->productsService->update($this->product->id, array(
             'name' => 'Another Product',
             'price' => 10,
-            'discount' => '10%'
         ));
 
         $product = $this->productsService->show($this->product->id);
 
         $this->assertNotEquals($product->name, $this->product->name);
         $this->assertEquals($product->price, 10);
-        $this->assertEquals($product->discount_price, 9);
         $this->assertEquals($this->product->supplier_id, $product->supplier_id);
     }
 
