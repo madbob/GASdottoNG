915ee86b5c3d0b5a4394ee697cd30723eafaa5be
Roberto Guido <bob4job@gmail.com>
2021-11-29 03:06:42 +0100

    ripristinato fix database per tipi movimenti contabili

--- code/app/Console/Commands/FixDatabase.php
+++ code/app/Console/Commands/FixDatabase.php
@@ -10,12 +10,12 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-use Artisan;
 use DB;
+use Artisan;
 
-use App\MovementType;
-use App\Config;
 use App\Gas;
+use App\Config;
+use App\MovementType;
 
 class FixDatabase extends Command
 {
@@ -36,6 +36,32 @@ class FixDatabase extends Command
         */
         Artisan::call('db:seed', ['--force' => true, '--class' => 'MovementTypesSeeder']);
         Artisan::call('db:seed', ['--force' => true, '--class' => 'ModifierTypesSeeder']);
+
+        $movements = MovementType::where('function', 'like', '%suppliers%')->get();
+        foreach($movements as $movement) {
+            $functions = json_decode($movement->function);
+            $new_functions = [];
+
+            foreach($functions as $function) {
+                foreach(['sender', 'target', 'master'] as $target) {
+                    $new_operations = [];
+                    $operations = $function->$target ?? (object) ['operations' => []];
+
+                    foreach($operations->operations as $operation) {
+                        if ($operation->field != 'suppliers') {
+                            $new_operations[] = $operation;
+                        }
+                    }
+
+                    $function->$target->operations = $new_operations;
+                }
+
+                $new_functions[] = $function;
+            }
+
+            $movement->function = json_encode($new_functions);
+            $movement->save();
+        }
 
         $gas = Gas::all();
         foreach(Config::customMailTypes() as $identifier => $metadata) {
