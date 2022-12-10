4c04775c933bf90ef2df507bff2c2bd8489a2248
Roberto Guido <info@madbob.org>
2022-12-10 16:18:48 +0100

    soppressi controller rest. mai realmente sviluppati, testati ed utilizzati

--- code/routes/api.php
+++ code/routes/api.php
@@ -1,11 +1,3 @@
 <?php
 
-use Illuminate\Http\Request;
-
-Route::group(array('prefix' => '1'), function () {
-    Route::get('users/search', 'REST\UsersController@search');
-    Route::resource('users', 'REST\UsersController');
-    Route::resource('suppliers', 'REST\SuppliersController');
-    Route::resource('products', 'REST\ProductsController');
-    Route::resource('vatrates', 'REST\VatRatesController');
-});
+// dummy
