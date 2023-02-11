e5899dcdcfe406f1c39ba9a5112a503abda565dc
Roberto Guido <info@madbob.org>
2023-02-11 22:22:14 +0100

    peso in dettaglio prodotti. closes #185

--- code/resources/views/product/show.blade.php
+++ code/resources/views/product/show.blade.php
@@ -27,33 +27,16 @@
             <div class="row">
                 <div class="col-md-6">
                     <x-larastrap::text name="portion_quantity" :label="_i('Pezzatura')" readonly disabled />
+					<x-larastrap::text name="package_size" :label="_i('Confezione')" readonly disabled />
+					<x-larastrap::text name="min_quantity" :label="_i('Minimo')" readonly disabled />
+					<x-larastrap::text name="max_available" :label="_i('Disponibile')" readonly disabled />
+					<x-larastrap::text name="weight" :label="_i('Peso')" readonly disabled />
                 </div>
                 <div class="col-md-6">
                     <x-larastrap::check name="variable" :label="_i('Variabile')" readonly disabled />
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <x-larastrap::text name="package_size" :label="_i('Confezione')" readonly disabled />
-                </div>
-                <div class="col-md-6">
-                    <x-larastrap::text name="multiple" :label="_i('Multiplo')" readonly disabled />
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <x-larastrap::text name="min_quantity" :label="_i('Minimo')" readonly disabled />
-                </div>
-                <div class="col-md-6">
-                    <x-larastrap::text name="max_quantity" :label="_i('Massimo')" readonly disabled />
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <x-larastrap::text name="max_available" :label="_i('Disponibile')" readonly disabled />
+					<x-larastrap::text name="multiple" :label="_i('Multiplo')" readonly disabled />
+					<x-larastrap::text name="max_quantity" :label="_i('Massimo')" readonly disabled />
+					<x-larastrap::text name="global_min" :label="_i('Minimo Complessivo')" readonly disabled />
                 </div>
             </div>
 
