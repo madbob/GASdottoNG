0f768455df04e6eee96c83e49692bee1172afdca
Roberto Guido <info@madbob.org>
2023-07-16 23:20:17 +0200

    qualche fix per mobile. closes #204

--- code/resources/views/commons/detailsbutton.blade.php
+++ code/resources/views/commons/detailsbutton.blade.php
@@ -1,6 +1,6 @@
 @if($obj && $obj->getROShowURL() != null)
     {{-- Questo deve essere coerente con la funzione detailsButton() in utils.js --}}
-    <button type="button" class="btn btn-xs btn-icon btn-info object-details d-none d-md-inline-block" data-show-url="{{ $obj->getROShowURL() }}">
+    <button type="button" class="btn btn-xs btn-icon btn-info object-details d-none d-xl-inline-block" data-show-url="{{ $obj->getROShowURL() }}">
         <i class="bi-zoom-in"></i>
     </button>
 @endif
