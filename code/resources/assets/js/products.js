80615ca026c3e00a6a3d38577ce0d876407f51b6
Roberto Guido <info@madbob.org>
2023-10-30 15:44:41 +0100

    operazioni multiple in pannello di modifica rapida prodotti. closes #226

--- code/resources/assets/js/products.js
+++ code/resources/assets/js/products.js
@@ -12,12 +12,40 @@ class Products {
             rapida
         */
         if (container.hasClass('products-grid')) {
-            container.on('active-filter', (e) => {
-                container.find('.category-toggle').removeClass('hidden');
+            container.on('change', '.product-select', (e) => {
+                let count = container.find('.product-select').filter(':checked').length;
+                container.find('.massive-actions').toggleClass('hidden', count == 0);
             });
 
-            container.on('inactive-filter', (e) => {
-                container.find('.category-toggle').addClass('hidden');
+            container.on('change', '.massive-actions select', (e) => {
+                let sel = $(e.currentTarget);
+                let value = sel.val();
+                let offset = sel.closest('th').index();
+
+                container.find('.product-select').filter(':checked').each((index, item) => {
+                    $(item).closest('tr').find('td').eq(offset).find('select').val(value);
+                });
+            });
+
+            container.on('change', '.massive-actions input[type=checkbox]', (e) => {
+                let check = $(e.currentTarget);
+                let value = check.prop('checked');
+                let offset = check.closest('th').index();
+
+                container.find('.product-select').filter(':checked').each((index, item) => {
+                    $(item).closest('tr').find('td').eq(offset).find('input[type=checkbox]').prop('checked', value);
+                });
+            });
+
+            container.on('click', '.massive-actions .remove_all', (e) => {
+                e.preventDefault();
+
+                container.find('.product-select').filter(':checked').each((index, item) => {
+                    let row = $(item).closest('tr');
+                    let id = row.find('input[type=hidden][name="id[]"]').val();
+                    let remove = $('<input>').attr('type', 'hidden').attr('name', 'remove[]').attr('value', id);
+                    row.addClass('table-danger').find('td').eq(1).append(remove);
+                });
             });
         }
     }
