4f58020ec3d036c30cfc23fb9b711224decfe3e7
Roberto Guido <info@madbob.org>
2023-11-08 16:42:19 +0100

    qualche ottimizzazione

--- code/app/Providers/SingletonsProvider.php
+++ code/app/Providers/SingletonsProvider.php
@@ -13,7 +13,20 @@ class SingletonsProvider extends ServiceProvider
 
     public function register()
     {
-        $classes = classesInNamespace('App\\Singletons');
+        /*
+            Questo si suppone essere l'elenco di tutte le classi in
+            app/Singletons: da tenere aggiornato!
+        */
+        $classes = [
+            \App\Singletons\AggregationSwitch::class,
+            \App\Singletons\GlobalScopeHub::class,
+            \App\Singletons\LogHarvester::class,
+            \App\Singletons\ModifierEngine::class,
+            \App\Singletons\MovementsHub::class,
+            \App\Singletons\OrderNumbersDispatcher::class,
+            \App\Singletons\RemoteRepository::class,
+            \App\Singletons\TempCache::class,
+        ];
 
         foreach($classes as $class) {
             $this->app->singleton(class_basename($class), function ($app) use ($class) {
