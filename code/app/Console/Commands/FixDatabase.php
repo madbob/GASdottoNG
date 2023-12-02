47694059bc95e8ecb6a56bd8a39a44c6044d8329
Roberto Guido <info@madbob.org>
2023-12-02 18:15:21 +0100

    ripristinate funzioni in script di fix database

--- code/app/Console/Commands/FixDatabase.php
+++ code/app/Console/Commands/FixDatabase.php
@@ -12,7 +12,11 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Artisan;
 
+use App\Gas;
+use App\User;
+use App\Supplier;
 use App\ModifierType;
+use App\Role;
 
 class FixDatabase extends Command
 {
@@ -28,6 +32,65 @@ class FixDatabase extends Command
         */
         Artisan::call('db:seed', ['--force' => true, '--class' => 'MovementTypesSeeder']);
         Artisan::call('db:seed', ['--force' => true, '--class' => 'ModifierTypesSeeder']);
+
+        /*
+            Per fixare gli URL dei listini autogenerati ed erroneamente
+            sovrascritti
+        */
+        foreach(Supplier::all() as $supplier) {
+            $attachments = $supplier->attachments;
+
+            $pdf = $attachments->firstWhere('name', 'Listino PDF (autogenerato)');
+            if ($pdf) {
+                $pdf->url = route('suppliers.catalogue', ['id' => $supplier->id, 'format' => 'pdf']);
+                $pdf->save();
+            }
+
+            $csv = $attachments->firstWhere('name', 'Listino CSV (autogenerato)');
+            if ($csv) {
+                $csv->url = route('suppliers.catalogue', ['id' => $supplier->id, 'format' => 'csv']);
+                $csv->save();
+            }
+        }
+
+        /*
+            Per non attivare il tour di onboarding per gli utenti esistenti
+        */
+        User::query()->update(['tour' => true]);
+
+        /*
+            Per impostare un default sull'approvazione manuale delle
+            registrazioni pubbliche degli utenti
+        */
+        foreach(Gas::all() as $gas) {
+            $registrations_info = $gas->public_registrations;
+            if (isset($registrations_info['manual']) == false) {
+                $registrations_info['manual'] = false;
+                $gas->setConfig('public_registrations', $registrations_info);
+            }
+        }
+
+        /*
+            Per azzerare le vecchie configurazioni Satispay non compatibili con
+            la nuova implementazione
+        */
+        foreach(Gas::all() as $gas) {
+            $satispay_info = $gas->satispay;
+            if (isset($satispay_info['public']) == false) {
+                $satispay_info = (object) [
+                    'public' => '',
+                    'secret' => '',
+                    'key' => '',
+                ];
+
+                $gas->setConfig('satispay', $satispay_info);
+            }
+        }
+
+        /*
+            Per assegnare degli identificativi ai modificatori di default,
+            compatibili con l'importazione GDXP
+        */
 
         $mod = ModifierType::where('name', 'Sconto')->where('system', true)->where('identifier', '')->first();
         if ($mod) {
