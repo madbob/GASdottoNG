7ec721016af8e44e37739ddee9985178434b04dd
PixelTux <pixeltux@proton.me>
2025-01-26 20:36:58 +0100

    clean up code comment

--- code/database/migrations/2025_01_25_160007_user_purge.php
+++ code/database/migrations/2025_01_25_160007_user_purge.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('purge')->default(false); // Adds a nullable 'aba' column
+            $table->boolean('purge')->default(false);
         });
     }
 
