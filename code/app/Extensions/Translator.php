733305e808365e8547bdea9547bd0bda604757df
Roberto Guido <info@madbob.org>
2026-02-08 23:31:20 +0100

    piccola correzione per traduzioni

--- code/app/Extensions/Translator.php
+++ code/app/Extensions/Translator.php
@@ -16,10 +16,8 @@ class Translator extends OriginalTranslator
     public function get($key, array $replace = [], $locale = null, $fallback = true)
     {
         $line = parent::get($key, $replace, $locale, $fallback);
-        if (is_string($line)) {
-            if (blank($line)) {
-                return $this->get($key, $replace, config('app.fallback_locale'), false);
-            }
+        if ($line == $key || blank($line)) {
+            $line = parent::get($key, $replace, config('app.fallback_locale'), false);
         }
 
         return $line;
