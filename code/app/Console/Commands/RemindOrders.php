a0c2609b41b98c1e55b2384836b9f69848a9860b
Roberto Guido <info@madbob.org>
2026-04-04 17:14:22 +0200

    razionalizzato comando notifica ordini in chiusura

--- code/app/Console/Commands/RemindOrders.php
+++ code/app/Console/Commands/RemindOrders.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Log;
 
 use Carbon\Carbon;
 
-use App\Jobs\NotifyRemindOrder;
+use App\Notifications\RemindOrderNotification;
 use App\Gas;
 use App\Order;
 
@@ -17,9 +17,55 @@ class RemindOrders extends Command
 
     protected $description = 'Invia le notifiche di promemoria per gli ordini';
 
-    public function handle()
+    private function deliverMails($aggregate_users)
     {
-        $today = Carbon::today();
+        Log::info('Invio notifica chiusura ordini a ' . count($aggregate_users) . ' utenti');
+        foreach ($aggregate_users as $auser) {
+            try {
+                if (filled($auser->user->email)) {
+                    $auser->user->notify(new RemindOrderNotification($auser->orders));
+                }
+            }
+            catch (\Exception $e) {
+                \Log::error('Impossibile inoltrare mail di promemoria ordine a ' . $auser->user->email . ': ' . $e->getMessage());
+            }
+        }
+    }
+
+    private function aggregateNotifications($notifications, $today_formatted)
+    {
+        $hub = app()->make('GlobalScopeHub');
+
+        foreach ($notifications as $gas_id => $data) {
+            Log::info('Invio promemoria per ordini ' . join(', ', array_map(fn($o) => $o->id, $data->orders)));
+            $hub->setGas($data->gas->id);
+
+            foreach ($data->orders as $order) {
+                foreach ($order->aggregate->gas as $gas) {
+                    $aggregate_users = [];
+                    $users = $order->notifiableUsers($gas);
+
+                    foreach ($users as $user) {
+                        if (isset($aggregate_users[$user->id]) === false) {
+                            $aggregate_users[$user->id] = (object) [
+                                'user' => $user,
+                                'orders' => [],
+                            ];
+                        }
+
+                        $aggregate_users[$user->id]->orders[] = $order;
+                    }
+
+                    $this->deliverMails($aggregate_users);
+                }
+            }
+
+            $data->gas->setConfig('last_sent_order_reminder', $today_formatted);
+        }
+    }
+
+    private function filterNotifiable($today, $today_formatted)
+    {
         $today_formatted = $today->format('Y-m-d');
         $orders = Order::where('status', 'open')->where('end', '>', $today)->get();
         $notifications = [];
@@ -39,25 +85,25 @@ class RemindOrders extends Command
 
                 if ($order->end->format('Y-m-d') == $expiration->format('Y-m-d')) {
                     if (isset($notifications[$gas->id]) === false) {
-                        $notifications[$gas->id] = [];
+                        $notifications[$gas->id] = (object) [
+                            'gas' => $gas,
+                            'orders' => [],
+                        ];
                     }
 
-                    $notifications[$gas->id][] = $order->id;
+                    $notifications[$gas->id]->orders[] = $order;
                 }
             }
         }
 
-        $hub = app()->make('GlobalScopeHub');
+        return $notifications;
+    }
 
-        foreach ($notifications as $gas_id => $orders) {
-            Log::info('Invio promemoria per ordini ' . implode(', ', $orders));
-            $hub->setGas($gas_id);
-            NotifyRemindOrder::dispatch($orders);
-        }
-
-        $gas = Gas::all();
-        foreach ($gas as $g) {
-            $g->setConfig('last_sent_order_reminder', $today_formatted);
-        }
+    public function handle()
+    {
+        $today = Carbon::today();
+        $today_formatted = $today->format('Y-m-d');
+        $notifications = $this->filterNotifiable($today, $today_formatted);
+        $this->aggregateNotifications($notifications, $today_formatted);
     }
 }
