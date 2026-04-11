1221f8d1103fc977530d7f706a3df432db48ffa2
Roberto Guido <info@madbob.org>
2026-04-11 16:23:28 +0200

    prevenzione ID vuoti

--- code/app/SluggableID.php
+++ code/app/SluggableID.php
@@ -15,6 +15,17 @@ trait SluggableID
         while (true) {
             $slug = Str::slug($this->name) . $append;
 
+            /*
+                Questo è per prevenire i (rari, ma possibili) casi in cui si
+                cerca di creare una istanza con un nome vuoto. In tal caso anche
+                l'ID sarebbe vuoto, cosa che spacca poi la costruzione degli URL
+                e, di conseguenza, genera problemi apparentemente random nel
+                frontend
+            */
+            if (blank($slug)) {
+                $slug = Str::random(10);
+            }
+
             if ($classname::withoutGlobalScope('gas')->where('id', $slug)->first() != null) {
                 $append = '_' . $index;
                 $index++;
