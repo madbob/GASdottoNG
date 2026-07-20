d38f3c961b255a191f65cf7b94381553fbddc04c
Roberto Guido <info@madbob.org>
2026-07-20 21:53:06 +0200

    fix in salvataggio aggregazioni/gruppi dell'utente

--- code/app/Models/Concerns/InCircles.php
+++ code/app/Models/Concerns/InCircles.php
@@ -14,13 +14,13 @@ trait InCircles
         return $this->belongsToMany(Circle::class)->with('group');
     }
 
-    /*
-        Restituisce un array con i Circle assegnati, suddivisi per Group di
-        riferimento.
-        Se uno specifico Group viene richiesto, torna solo i relativi Circles.
-        Se nessun Circle è stato assegnato per quel Group, ritorna comunque una
-        struttura dati valida
-    */
+    /**
+     * Restituisce un array con i Circle assegnati, suddivisi per Group di
+     * riferimento.
+     * Se uno specifico Group viene richiesto, torna solo i relativi Circles.
+     * Se nessun Circle è stato assegnato per quel Group, ritorna comunque una
+     * struttura dati valida
+     */
     public function circlesByGroup($group = null)
     {
         $ret = [];
@@ -58,19 +58,44 @@ trait InCircles
         return $ret;
     }
 
+    /**
+     * Trasforma una request nella relativa associazione/disassociazione in
+     * aggregazioni e gruppi. In $request ci si aspetta sia l'elenco delle
+     * aggregazioni da considerare, ed il relativo elenco di gruppi di
+     * abilitare.
+     * Da ricordare che quando un utente salva il proprio profilo non
+     * necessariamente può modificare tutte le sue relative aggregazioni, dunque
+     * non basta fare un sync() sulle relazioni: aggregazione per aggregazione
+     * occorre vedere cosa attaccare e cosa sganciare, se non ci sono
+     * aggregazioni nella $request vuol dire che non poteva essere modificato
+     * nulla e dunque nulla c'è da cambiare
+     */
     public function readCircles($request)
     {
         $groups = $request['groups'] ?? [];
         $groups = Group::whereIn('id', $groups)->get();
 
-        $circles = [];
-
         foreach ($groups as $group) {
             $key = sprintf('circles__%s__%s', sanitizeId($this->id), sanitizeId($group->id));
-            $circles = array_merge($circles, $request[$key] ?? []);
-        }
+            $circles = $request[$key] ?? [];
+            $existing = [];
 
-        $this->circles()->sync($circles);
+            foreach($this->circles as $attached) {
+                if ($attached->group_id == $group->id) {
+                    if (in_array($attached->id, $circles)) {
+                        $existing[] = $attached->id;
+                    }
+                    else {
+                        $this->circles()->detach($attached->id);
+                    }
+                }
+            }
+
+            $missing = array_diff($circles, $existing);
+            foreach($missing as $miss) {
+                $this->circles()->attach($miss);
+            }
+        }
     }
 
     abstract public function eligibleGroups();
